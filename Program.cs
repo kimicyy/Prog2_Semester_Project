983cf75f69210b9a3b56645f6a6f8709104f52c3
kimicyy <kimicyy@gmail.com>
2023-08-03 19:51:41 +0800

    Set up basic namespaces and invent Calendar class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Linq;
+using static System.Console;
+using static System.Math;
+
+class Calendar{
+    static void Main(){
+
+    }
+}

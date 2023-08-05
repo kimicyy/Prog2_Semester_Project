b7364c94afe1e12b1d99153cf11ce5f71e835403
kimicyy <kimicyy@gmail.com>
2023-08-06 01:19:48 +0800

    Finish Event class

--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@ using static System.Console;
 using static System.Math;
 
 class Event{
-    double priority;
-    string eventName;
-    var priorityStrToDouble = new Dictionary<string, double>{
-        {  }
+    public double priority;
+    public string eventName;
+    public Dictionary<string, double> priorityStrToDouble = new Dictionary<string, double>{
+        { "Urgent", 10.0 }, { "Normal", 5.0 }, { "Ignorable", 1.0 }
     };
     public Event(string eventName, string priorityStr){
-        
+        this.priority = priorityStrToDouble[priorityStr];
+        this.eventName = eventName;
     }
 }
 

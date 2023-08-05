6d3a7d36ced2322c38ce3a6bca1672b2692d99dc
kimicyy <kimicyy@gmail.com>
2023-08-06 01:26:58 +0800

    Fix Event class dictionary

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ class Event{
     public double priority;
     public string eventName;
     public Dictionary<string, double> priorityStrToDouble = new Dictionary<string, double>{
-        { "Urgent", 10.0 }, { "Normal", 5.0 }, { "Ignorable", 1.0 }
+        { "Urgent", 10.0 }, 
+        { "Normal", 5.0 }, 
+        { "Trivial", 1.0 }
     };
     public Event(string eventName, string priorityStr){
         this.priority = priorityStrToDouble[priorityStr];

0dabe5ae6c1c976dafee74fbe41f356752887d23
kimicyy <kimicyy@gmail.com>
2023-08-07 15:56:01 +0800

    Add fields to class Event

--- Program.cs
+++ Program.cs
@@ -9,18 +9,49 @@ using static System.Math;
 class Event{
     public double priority;
     public string eventName;
+    public int startYear; 
+    public int startMonth;
+    public int startHour;
+    public int startMin;
+    public int startSec;
+    public int endYear;
+    public int endMonth;
+    public int endHour;
+    public int endMin;
+    public int endSec;
+    public string? sideNote; 
     public Dictionary<string, double> priorityStrToDouble = new Dictionary<string, double>{
-        { "Urgent", 10.0 }, 
-        { "Normal", 5.0 }, 
-        { "Trivial", 1.0 }
+        { "Urgent", 10.0 }, { "Normal", 5.0 }, { "Ignorable", 1.0 }
     };
-    public Event(string eventName, string priorityStr){
+
+    public Event(string eventName, string priorityStr, int startYear, int startMonth, int startHour, int startMin, int startSec,
+                    int endYear, int endMonth, int endHour, int endMin, int endSec, string? sideNote = null){
         this.priority = priorityStrToDouble[priorityStr];
         this.eventName = eventName;
+        this.startYear = startYear;
+        this.startMonth = startMonth;
+        this.startHour = startHour;
+        this.startMin = startMin;
+        this.startSec = startSec;
+        this.endYear = endYear;
+        this.endMonth = endMonth;
+        this.endHour = endHour;
+        this.endMin = endMin;
+        this.endSec = endSec;
+        this.sideNote = sideNote;
     }
 }
 
 class Calendar{
+    PriorityQueue<Event, int> events;
+
+    public Calendar(){
+        this.events = new PriorityQueue<Event, int>();
+    }
+
+    public void addEvent(Event e){
+    }
+
     static void Main(){
 
     }

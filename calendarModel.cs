a0560cd212cf939f8d12c9b2d32475c5720a659d
kimicyy <kimicyy@gmail.com>
2023-08-14 23:25:47 +0800

    Change priority in class Event reversely and modify addEvent method

--- calendarModel.cs
+++ calendarModel.cs
@@ -8,26 +8,29 @@ using static System.Math;
 
 class Event{
     public double priority;
+    public string priorityStr;
     public string eventName;
     public int startYear; 
     public int startMonth;
-    public int startDay
+    public int startDay;
     public int startHour;
     public int startMin;
     public int startSec;
     public int endYear;
     public int endMonth;
+    public int endDay;
     public int endHour;
     public int endMin;
     public int endSec;
     public string? sideNote; 
     public Dictionary<string, double> priorityStrToDouble = new Dictionary<string, double>{
-        { "Urgent", 10.0 }, { "Normal", 5.0 }, { "Trivial", 1.0 }
+        { "Urgent", 1.0 }, { "Normal", 5.0 }, { "Trivial", 10.0 }
     };
 
     public Event(string eventName, string priorityStr, int startYear, int startMonth, int startDay, 
                     int startHour, int startMin, int startSec, int endYear, int endMonth, int endDay, 
                     int endHour, int endMin, int endSec, string? sideNote = null){
+        this.priorityStr = priorityStr;
         this.priority = priorityStrToDouble[priorityStr];
         this.eventName = eventName;
         this.startYear = startYear;
@@ -47,25 +50,74 @@ class Event{
 }
 
 class Calendar{
-    PriorityQueue<Event, double> events;
-    List<Event> eventList;
-    List<Event> eventListForSchedule;
+    public PriorityQueue<Event, double> events;
+    public List<Event> eventList;
 
     public Calendar(){
         this.events = new PriorityQueue<Event, double>();
         this.eventList = new List<Event>();
-    }
+        }
 
     public void addEvent(Event eve){
-        events.Enqueue(eve, eve.priority);
-        eventList.Add(eve);
+        var eveStartDatetime = Convert.ToDateTime($"{eve.startYear}-{eve.startMonth}-{eve.startDay} {eve.startHour}:{eve.startMin}:{eve.startSec}");
+        var eveEndDatetime = Convert.ToDateTime($"{eve.endYear}-{eve.endMonth}-{eve.endDay} {eve.endHour}:{eve.endMin}:{eve.endSec}");
+        foreach (var e in eventList){
+            var eStartDatetime = Convert.ToDateTime($"{e.startYear}-{e.startMonth}-{e.startDay} {e.startHour}:{e.startMin}:{e.startSec}");
+            var eEndDatetime = Convert.ToDateTime($"{e.endYear}-{e.endMonth}-{e.endDay} {e.endHour}:{e.endMin}:{e.endSec}");
+            bool samePriority = false;
+            // overlapping detected
+            if ((eveStartDatetime > eStartDatetime && eveEndDatetime > eEndDatetime) ||
+                (eveStartDatetime < eStartDatetime && eveEndDatetime < eEndDatetime) ||
+                (eveStartDatetime > eStartDatetime && eveEndDatetime < eEndDatetime) ||
+                (eveStartDatetime < eStartDatetime && eveEndDatetime > eEndDatetime) ||
+                (eveStartDatetime == eStartDatetime && eveEndDatetime == eEndDatetime)){
+                    WriteLine($"Event {eve.eventName} and event {e.eventName} overlapping detected!");
+                    if (eve.priority < e.priority){
+                        WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
+                        WriteLine($"Event {e.eventName}: {e.priorityStr}");
+                        WriteLine($"Event {e.eventName} will be deleted since it has lower priority");
+                        eventList.Add(eve);
+                        eventList.Remove(e);
+                        return;
+                    }
+                    else if (eve.priority > e.priority){
+                        WriteLine($"Event {e.eventName}: {e.priorityStr}");
+                        WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
+                        WriteLine($"Event {eve.eventName} will be deleted since it has lower priority");
+                        return;
+                    }
+                    else{
+                        samePriority = true;
+                    }
+                if (samePriority){
+                    WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
+                    WriteLine($"Event {e.eventName}: {e.priorityStr}");
+                    WriteLine($"Event {eve.eventName} and Event {e.eventName} have the same priority");
+                    WriteLine($"Please choose which event to delete! (1 for {eve.eventName}, 2 for {e.eventName})");
+                    if (ReadLine()! == "2"){
+                        eventList.Add(eve);
+                        eventList.Remove(e);
+                    }
+                    return;
+                }
+            }
+            eventList.Add(eve);
+        }
     }
-    
+    public void generateSchedule(){
+        foreach (var e in eventList){
+            events.Enqueue(e, e.priority);
+        }
+        while (events.Count > 0){
+            var eve = events.Dequeue();
+            WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
+        }
+    }
 }
 
 class User{
-    string name;
-    Calendar calendar;
+    public string name;
+    public Calendar calendar;
     public User(string name){
         this.name = name;
         this.calendar = new Calendar();

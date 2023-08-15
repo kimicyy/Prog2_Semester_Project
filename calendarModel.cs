aa12caed067af013b35174fa832e3ce24e2f7e37
kimicyy <kimicyy@gmail.com>
2023-08-15 14:59:50 +0800

    Fix the conditions of addEvent though there are bugs

--- calendarModel.cs
+++ calendarModel.cs
@@ -47,6 +47,16 @@ class Event{
         this.endSec = endSec;
         this.sideNote = sideNote;
     }
+
+    public static bool eventsEqual(Event e0, Event e1){
+        return (e0.eventName.Equals(e1.eventName) && e0.priorityStr.Equals(e1.priorityStr) &&
+                e0.startYear.Equals(e1.startYear) && e0.startMonth.Equals(e1.startMonth) && 
+                e0.startDay.Equals(e1.startDay) && e0.startHour.Equals(e1.startHour) &&
+                e0.startMin.Equals(e1.startMin) && e0.startSec.Equals(e1.startSec) && 
+                e0.endYear.Equals(e1.endYear) && e0.endMonth.Equals(e1.endMonth) && 
+                e0.endDay.Equals(e1.endDay) && e0.endHour.Equals(e1.endHour) && 
+                e0.endMin.Equals(e1.endMin) && e0.endSec.Equals(e1.endSec));
+    }
 }
 
 class Calendar{
@@ -61,48 +71,58 @@ class Calendar{
     public void addEvent(Event eve){
         var eveStartDatetime = Convert.ToDateTime($"{eve.startYear}-{eve.startMonth}-{eve.startDay} {eve.startHour}:{eve.startMin}:{eve.startSec}");
         var eveEndDatetime = Convert.ToDateTime($"{eve.endYear}-{eve.endMonth}-{eve.endDay} {eve.endHour}:{eve.endMin}:{eve.endSec}");
-        foreach (var e in eventList){
+        // TODO: fix adding events
+        for (int i = 0; true; i++){
+            if (i >= eventList.Count){
+                break;
+            }
+            var e = eventList[i];
             var eStartDatetime = Convert.ToDateTime($"{e.startYear}-{e.startMonth}-{e.startDay} {e.startHour}:{e.startMin}:{e.startSec}");
             var eEndDatetime = Convert.ToDateTime($"{e.endYear}-{e.endMonth}-{e.endDay} {e.endHour}:{e.endMin}:{e.endSec}");
             bool samePriority = false;
+            if (Event.eventsEqual(eve, e)){
+                continue;
+            }
             // overlapping detected
-            if ((eveStartDatetime > eStartDatetime && eveEndDatetime > eEndDatetime) ||
-                (eveStartDatetime < eStartDatetime && eveEndDatetime < eEndDatetime) ||
-                (eveStartDatetime > eStartDatetime && eveEndDatetime < eEndDatetime) ||
+            if ((eveStartDatetime > eStartDatetime && eveStartDatetime < eEndDatetime && eveEndDatetime > eEndDatetime) ||
+                (eveStartDatetime < eStartDatetime && eveEndDatetime > eStartDatetime && eveEndDatetime < eEndDatetime) ||
                 (eveStartDatetime < eStartDatetime && eveEndDatetime > eEndDatetime) ||
+                (eveStartDatetime > eStartDatetime && eveEndDatetime < eEndDatetime) ||
                 (eveStartDatetime == eStartDatetime && eveEndDatetime == eEndDatetime)){
-                    WriteLine($"Event {eve.eventName} and event {e.eventName} overlapping detected!");
+                    WriteLine($"Event \"{eve.eventName}\" and event \"{e.eventName}\" overlapping detected!");
                     if (eve.priority < e.priority){
-                        WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
-                        WriteLine($"Event {e.eventName}: {e.priorityStr}");
-                        WriteLine($"Event {e.eventName} will be deleted since it has lower priority");
+                        WriteLine($"Event \"{eve.eventName}\": {eve.priorityStr}");
+                        WriteLine($"Event \"{e.eventName}\": {e.priorityStr}");
+                        WriteLine($"Event \"{e.eventName}\" will be deleted since it has lower priority");
                         eventList.Add(eve);
                         eventList.Remove(e);
-                        return;
+                        continue;
                     }
                     else if (eve.priority > e.priority){
-                        WriteLine($"Event {e.eventName}: {e.priorityStr}");
-                        WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
-                        WriteLine($"Event {eve.eventName} will be deleted since it has lower priority");
+                        WriteLine($"Event \"{e.eventName}\": {e.priorityStr}");
+                        WriteLine($"Event \"{eve.eventName}\": {eve.priorityStr}");
+                        WriteLine($"Event \"{eve.eventName}\" will be deleted since it has lower priority");
                         return;
                     }
                     else{
                         samePriority = true;
                     }
                 if (samePriority){
-                    WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
-                    WriteLine($"Event {e.eventName}: {e.priorityStr}");
-                    WriteLine($"Event {eve.eventName} and Event {e.eventName} have the same priority");
-                    WriteLine($"Please choose which event to delete! (1 for {eve.eventName}, 2 for {e.eventName})");
+                    WriteLine($"Event \"{eve.eventName}\": {eve.priorityStr}");
+                    WriteLine($"Event \"{e.eventName}\": {e.priorityStr}");
+                    WriteLine($"Event \"{eve.eventName}\" and Event \"{e.eventName}\" have the same priority");
+                    WriteLine($"Please choose which event to delete! (1 for \"{eve.eventName}\", 2 for \"{e.eventName}\")");
                     if (ReadLine()! == "2"){
                         eventList.Add(eve);
                         eventList.Remove(e);
                     }
-                    return;
+                    else{
+                        return;
+                    }
                 }
             }
-            eventList.Add(eve);
         }
+        eventList.Add(eve);
     }
     public void generateSchedule(){
         foreach (var e in eventList){
@@ -112,6 +132,7 @@ class Calendar{
             var eve = events.Dequeue();
             WriteLine($"Event {eve.eventName}: {eve.priorityStr}");
         }
+        // TODO: need to show the time span of events
     }
 }
 
@@ -126,6 +147,13 @@ class User{
 
 class Top{
     static void Main(){
-        
+        var user1 = new User("Kimi");
+        user1.calendar.addEvent(new Event("Make Lunch", "Urgent", 2023, 8, 14, 11, 0, 0, 
+        2023, 8, 14, 12, 0, 0));
+        user1.calendar.addEvent(new Event("Do Homework", "Trivial", 2023, 8, 14, 11, 0, 0, 
+        2023, 8, 14, 12, 0, 0));
+        user1.calendar.addEvent(new Event("Make Dinner", "Urgent", 2023, 8, 14, 18, 0, 0, 
+        2023, 8, 14, 19, 0, 0));
+        user1.calendar.generateSchedule();
     }
 }
